06826a1ea4fb5b4f7e23de11de8f2f5bf75f00c1
Tillmann Heidsieck <theidsieck@leenox.de>
2021-02-13 17:26:40 +0100

    alembic: add initial revision of database
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

---
+++ alembic/versions/8b3b42885014_initial_revision.py
@@ -0,0 +1,24 @@
+"""initial revision
+
+Revision ID: 8b3b42885014
+Revises: 
+Create Date: 2021-02-13 17:23:29.290621
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8b3b42885014'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
