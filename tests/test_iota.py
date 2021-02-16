674fd0302eab891604a4dfe112a1fed4f3f677eb
Tillmann Heidsieck <theidsieck@leenox.de>
2021-02-16 22:07:38 +0100

    iota: test: restrict to 80 chars per line
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- tests/test_iota.py
+++ tests/test_iota.py
@@ -10,11 +10,14 @@ import tempfile
 import pytest
 
 ADMIN_TOKEN = \
-    "QWw6kjrJY5xB4VSzWns+DZjM7Tda5CI9YlEmq43oTsQAeTHJpuG+gc4ZVr21hs+XkcXo5IQGixKV+QhUKhTdeA=="
+    "QWw6kjrJY5xB4VSzWns+DZjM7Tda5CI9YlEmq43oTsQAeTHJpuG+gc4ZVr21hs+XkcXo5IQGix\
+KV+QhUKhTdeA=="
 READER_TOKEN = \
-    "UbIwkJJ8ranX9FWcnALyAfq3du5dPG+CfNhjA++O+gjl+WQnonp4wSAavxwWt/48LlET+EowvbD9cbXgTT3Q2w=="
+    "UbIwkJJ8ranX9FWcnALyAfq3du5dPG+CfNhjA++O+gjl+WQnonp4wSAavxwWt/48LlET+Eowvb\
+D9cbXgTT3Q2w=="
 WRITER_TOKEN = \
-    "YOEN2RhX5kyMx+f/v1Jpg5B4p+R9u+XAvy9zP2x6yB7NkdutedlI++QMJpQ6qDOoyUW/PkpvY7lhLpe8yA1nRQ=="
+    "YOEN2RhX5kyMx+f/v1Jpg5B4p+R9u+XAvy9zP2x6yB7NkdutedlI++QMJpQ6qDOoyUW/PkpvY7\
+lhLpe8yA1nRQ=="
 
 
 def test_token_get(client):
@@ -36,7 +39,8 @@ def test_token_get(client):
     data = json.dumps({
         "name": "reader"
     })
-    reply = client.get('/api/v1/token', content_type="application/json", headers=headers, data=data)
+    reply = client.get('/api/v1/token', content_type="application/json",
+                       headers=headers, data=data)
     assert reply == 200
     assert b"reader" in reply.data
 
